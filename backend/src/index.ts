9d7ecabd6125a49b31b1e2b35a6e186684f6de56
Anmol-Gup <sp430661@email.com>
2025-12-23 20:39:14 +0530

    refactor: update code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,7 +25,7 @@ const storage = multer.memoryStorage()
 const upload = multer({
     storage,
     limits: {
-        fileSize: 5 * 1024 * 1024, // 10MB
+        fileSize: 5 * 1024 * 1024, // 5MB
     }
 })
 
